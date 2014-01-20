9cc82e0f6f5d69feb62aa006bb4165a5dafbb331
Chris Harrington <chrisharrington99@gmail.com>
2014-01-20 16:39:41 -0700

    User is now able to change the name of an issue.

--- IssueTracker.Web/Scripts/Views/IssueDetails.js
+++ IssueTracker.Web/Scripts/Views/IssueDetails.js
@@ -3,13 +3,16 @@
 
 	var _container;
 	var _oldDescription;
+	var _oldName;
 	var _descriptionFlipper;
+	var _nameFlipper;
 	var _transitioner = IssueTracker.Transitioner;
 
 	root.load = function (container) {
 		_container = container;
 		_descriptionFlipper = new IssueTracker.Controls.Flipper($("div.description div.flipper"));
-
+		_nameFlipper = new IssueTracker.Controls.Flipper($("div.name.flipper"));
+		
 		_setUpFlipPanels(container);
 		_hookupEvents(container);
 		_transitioner.load(IssueTracker.selectedIssue.statusId());
@@ -17,8 +20,10 @@
 
 	function _hookupEvents(container) {
 		container.on("click", "#save-description", _saveDescription);
-		container.on("click", "#cancel-description", _discardDescription);
+		container.on("click", "#discard-description", _discardDescription);
 		container.on("click", "div.transitions button", _executeTransition);
+		container.on("click", "#save-name", _saveName);
+		container.on("click", "#discard-name", _discardName);
 	}
 
 	function _executeTransition() {
@@ -30,6 +35,11 @@
 			_oldDescription = IssueTracker.selectedIssue.description();
 			_descriptionFlipper.toggle();
 		});
+
+		container.on("click", "div.name div.front", function () {
+			_oldName = IssueTracker.selectedIssue.name();
+			_nameFlipper.toggle();
+		});
 	};
 
 	function _discardDescription() {
@@ -40,15 +50,35 @@
 	function _saveDescription() {
 		var loader = _container.find("div.description img").show();
 		var buttons = _container.find("div.description button").attr("disabled", true);
-
-		$.post(IssueTracker.virtualDirectory() + "Issues/Update", _buildIssueParameters()).success(function() {
+		_updateIssue().done(function() {
 			_descriptionFlipper.toggle();
-		}).error(function() {
-			alert("An error has occurred while updating the issue's description. Please try again later.");
-		}).complete(function() {
+		}).fail(function() {
+			IssueTracker.Feedback.error("An error has occurred while updating the issue's description. Please try again later.");
+		}).always(function() {
 			loader.hide();
 			buttons.attr("disabled", false);
 		});
+	}
+
+	function _discardName() {
+		_nameFlipper.toggle();
+		IssueTracker.selectedIssue.name(_oldName);
+	}
+
+	function _saveName() {
+		var buttons = _container.find("div.name button").attr("disabled", true);
+		_updateIssue().done(function () {
+			_nameFlipper.toggle();
+			window.location.hash = window.location.hash.replace(_oldName.formatForUrl(), IssueTracker.selectedIssue.name().formatForUrl());
+		}).fail(function () {
+			IssueTracker.Feedback.error("An error has occurred while updating the issue's name. Please try again later.");
+		}).always(function () {
+			buttons.attr("disabled", false);
+		});
+	}
+
+	function _updateIssue() {
+		return $.post(IssueTracker.virtualDirectory() + "Issues/Update", _buildIssueParameters());
 	}
 
 	function _buildIssueParameters() {
