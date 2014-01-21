ae846ba92ad84ceed0e336a47a879ceb045574db
Chris Harrington <chrisharrington99@gmail.com>
2014-01-21 09:09:55 -0700

    IconButton helper now renders buttons with various options and attributes.

--- IssueTracker.Web/Helpers/ButtonHelper.cs
+++ IssueTracker.Web/Helpers/ButtonHelper.cs
@@ -4,29 +4,59 @@ namespace IssueTracker.Web.Helpers
 {
 	public static class ButtonHelper
 	{
-		public static MvcHtmlString IconButton(this HtmlHelper htmlHelper, string id, string text, string icon = null, string style = null)
+		public static MvcHtmlString IconButton(this HtmlHelper htmlHelper, string text, string icon = null, string style = null, bool isRight = false, string dataBind = null, string id = null, string labelDataBind = null)
 		{
 			var button = new TagBuilder("button");
-			button.MergeAttribute("id", id);
+
+			SetAttribute(button, "id", id);
+			SetAttribute(button, "data-bind", dataBind);
+			SetCssClasses(icon, style, isRight, button);
+
+			if (string.IsNullOrEmpty(icon))
+				button.SetInnerText(text);
+			else
+				AddInnerElements(text, icon, button, labelDataBind, isRight);
+
+			return MvcHtmlString.Create(button.ToString());
+		}
+
+		private static void AddInnerElements(string text, string icon, TagBuilder button, string labelDataBind, bool isRight)
+		{
+			var iconTag = new TagBuilder("i");
+			iconTag.AddCssClass("fa " + icon);
+
+			var labelTag = new TagBuilder("span");
+			labelTag.SetInnerText(text);
+			SetAttribute(labelTag, "data-bind", labelDataBind);
+
+			if (isRight)
+			{
+				button.InnerHtml += labelTag.ToString();
+				button.InnerHtml += iconTag.ToString();
+			}
+			else
+			{
+				button.InnerHtml += iconTag.ToString();
+				button.InnerHtml += labelTag.ToString();
+			}
+		}
+
+		private static void SetCssClasses(string icon, string style, bool isRight, TagBuilder button)
+		{
 			if (!string.IsNullOrEmpty(icon))
 				button.AddCssClass("icon");
 			if (!string.IsNullOrEmpty(style))
 				button.AddCssClass(style);
-			
-			if (string.IsNullOrEmpty(icon))
-				button.SetInnerText(text);
-			else
-			{
-				var iconTag = new TagBuilder("i");
-				iconTag.AddCssClass("fa " + icon);
-				button.InnerHtml += iconTag.ToString();
-
-				var labelTag = new TagBuilder("span");
-				labelTag.SetInnerText(text);
-				button.InnerHtml += labelTag.ToString();
-			}
-
-			return MvcHtmlString.Create(button.ToString());
+			if (isRight)
+				button.AddCssClass("right");
 		}
+
+		private static void SetAttribute(TagBuilder button, string name, string id)
+		{
+			if (!string.IsNullOrEmpty(id))
+				button.MergeAttribute(name, id);
+		}
+
+		
 	}
 }
