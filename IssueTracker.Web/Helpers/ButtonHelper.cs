e572566621f182ba19ac953971226f3427d5a7a2
Chris Harrington <chrisharrington99@gmail.com>
2014-01-21 08:55:54 -0700

    Tweaked blue. Added html helper for rendering an icon button. Added icon buttons to issue details page.

---
+++ IssueTracker.Web/Helpers/ButtonHelper.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+
+namespace IssueTracker.Web.Helpers
+{
+	public static class ButtonHelper
+	{
+		public static MvcHtmlString IconButton(this HtmlHelper htmlHelper, string id, string text, string icon = null, string style = null)
+		{
+			var button = new TagBuilder("button");
+			button.MergeAttribute("id", id);
+			if (!string.IsNullOrEmpty(icon))
+				button.AddCssClass("icon");
+			if (!string.IsNullOrEmpty(style))
+				button.AddCssClass(style);
+			
+			if (string.IsNullOrEmpty(icon))
+				button.SetInnerText(text);
+			else
+			{
+				var iconTag = new TagBuilder("i");
+				iconTag.AddCssClass("fa " + icon);
+				button.InnerHtml += iconTag.ToString();
+
+				var labelTag = new TagBuilder("span");
+				labelTag.SetInnerText(text);
+				button.InnerHtml += labelTag.ToString();
+			}
+
+			return MvcHtmlString.Create(button.ToString());
+		}
+	}
+}
