9cb2144bca1334d6e92388d2772ac366c6148d49
Chris Harrington <chrisharrington99@gmail.com>
2014-01-21 19:41:21 -0700

    Bug fixes for changing user names.

--- IssueTracker.SampleDataImporter/Program.cs
+++ IssueTracker.SampleDataImporter/Program.cs
@@ -19,7 +19,7 @@ namespace IssueTracker.SampleDataImporter
 	{
 		private const int NUM_ISSUES = 20;
 		private const int NUM_PROJECTS = 1;
-		private const string WORDS = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+		private const string WORDS = "Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum";
 
 		private static Random _random;
 		private static IContainer _container;
