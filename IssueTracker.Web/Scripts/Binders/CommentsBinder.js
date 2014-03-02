2f9f6670b45b567eea548bf30c0b838c816e36c8
Chris Harrington <chrisharrington99@gmail.com>
2014-03-02 00:22:16 -0700

    Comments binder now shows initial comments.

--- IssueTracker.Web/Scripts/Binders/CommentsBinder.js
+++ IssueTracker.Web/Scripts/Binders/CommentsBinder.js
@@ -5,8 +5,18 @@
 		init: function(element, valueAccessor) {
 			
 		},
-		update: function(element, valueAccessor) {
-			
+		update: function (element, valueAccessor) {
+			var params = valueAccessor();
+			var comments = params.list();
+			var html = $("#" + params.template).html();
+			if (comments.length == 0)
+				return;
+
+			element = $(element);
+			for (var i = 0; i < comments.length; i++) {
+				element.append(html);
+				ko.applyBindings(comments[i], element.find(">div:last")[0]);
+			}
 		}
 	};
 
