67333767494035d164f3b378006aceb835a5ae2f
Chris Harrington <chrisharrington99@gmail.com>
2014-03-10 11:48:28 -0600

    Added the UI for deleting a user.

---
+++ IssueTracker.Web/Scripts/Views/Users/DeleteUser.js
@@ -0,0 +1,27 @@
+﻿
+(function(root) {
+
+	root.user = ko.observable();
+	root.loading = ko.observable(false);
+
+	root.delete = function (user) {
+		root.user(user);
+		IssueTracker.Dialog.load("confirm-delete-user-template", root);
+	};
+
+	root.confirm = function () {
+		root.loading(true);
+		$.post(IssueTracker.virtualDirectory() + "Users/Delete", root.user()).done(function() {
+			IssueTracker.Feedback.success(root.user().name + " has been deleted.");
+		}).fail(function() {
+			IssueTracker.Feedback.error("An error has occurred while deleting the user. Please try again later.");
+		}).always(function() {
+			root.loading(false);
+		});
+	};
+
+	root.cancel = function() {
+		IssueTracker.Dialog.hide();
+	};
+
+})(root("IssueTracker.Users.DeleteUser"));
