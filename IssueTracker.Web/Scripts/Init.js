00db76148fefebfb977659380121bb076bc3003c
Chris Harrington <chrisharrington99@gmail.com>
2014-03-11 15:01:39 -0600

    Added nanobar hooked up to global ajax events.

--- IssueTracker.Web/Scripts/Init.js
+++ IssueTracker.Web/Scripts/Init.js
@@ -1,24 +1,46 @@
 ﻿
-(function(root) {
+(function (root) {
+
+	var _bar;
 
 	root.init = function () {
-		$("div.render-container").css("left", $(window).outerWidth() + "px");
+		_setupPath();
+		_setupHighcharts();
+		_setupNanobar();
+		_setupAjaxPrefilters();
 
+		IssueTracker.Header.init();
+		ko.applyBindings(IssueTracker);
+	};
+	
+	function _setupPath() {
 		Path.root("#/" + IssueTracker.selectedProject().name.formatForUrl() + "/issues");
 		Path.listen();
+	}
 
+	function _setupHighcharts() {
 		Highcharts.setOptions({
 			colors: ["#D42C2C", "#2F7ED8", "#FA9141", "#1AADCE", "#8BBC21", "#0D233A"]
 		});
+	}
 
-		IssueTracker.Header.init();
+	function _setupNanobar() {
+		_bar = new Nanobar({
+			bg: "#64A3ED"
+		});
+		
+		$(document).ajaxSend(function () {
+			_bar.go(15);
+		}).ajaxComplete(function () {
+			_bar.go(100);
+		});
+	}
 
+	function _setupAjaxPrefilters() {
 		$.ajaxPrefilter(function (options, original) {
 			if (IssueTracker.selectedProject)
 				options.data = $.param($.extend(original.data, { projectId: IssueTracker.selectedProject().id }));
 		});
-
-		ko.applyBindings(IssueTracker);
-	};
+	}
 
 })(root("IssueTracker.Init"));
