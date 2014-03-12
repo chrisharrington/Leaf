76c5c88c8a90940079f9597555c6f70e888b2c14
Chris Harrington <chrisharrington99@gmail.com>
2014-03-11 20:46:48 -0600

    When the user clicks a link to initiate a page transition, the current page remains in view until the new page has arrived from the server, at which point the new page transplants the old. This looks good now due to the loading bar at the top of the page.

--- IssueTracker.Web/Scripts/Binders/RemoteTemplateBinder.js
+++ IssueTracker.Web/Scripts/Binders/RemoteTemplateBinder.js
@@ -1,5 +1,5 @@
 
-(function() {
+(function () {
 
 	ko.bindingHandlers.remoteTemplate = {
 		update: function(element, valueAccessor) {
@@ -7,12 +7,11 @@
 			if (!view)
 				return;
 
-			$.when(_getDataFromView(view), _hideTiles($(element))).done(function (result) {
+			$.when(_getDataFromView(view)).then(function (result) {
 				if (!result)
 					return;
 
-				$(element).attr("class", "content-container " + view.style).empty().append($("<div></div>").addClass("binding-container").html(result[0])).find(">div.tile").css("visibility", "hidden");
-				_showTiles($(element));
+				$(element).attr("class", "content-container " + view.style).empty().append($("<div></div>").addClass("binding-container").html(result));
 
 				ko.cleanNode($(element).find(">div.binding-container")[0]);
 				ko.applyBindings(view.data, $(element).find(">div.binding-container")[0]);
@@ -31,46 +30,6 @@
 		return $.get(IssueTracker.virtualDirectory() + url).fail(function () {
 			IssueTracker.Feedback.error("An error occurred retrieving the view at " + url + ". Please contact technical support.");
 		});
-	}
-
-	function _hideTiles(element) {
-		var deferred = new $.Deferred();
-		var tileDeferreds = [];
-
-		$(element).find(">div.tile:visible").each(function() {
-			var inner = new $.Deferred();
-			tileDeferreds.push(inner.promise());
-			$(this).fadeOut(250, function() {
-				inner.resolve();
-			});
-		});
-
-		$.when.apply(null, tileDeferreds).then(function() {
-			deferred.resolve();
-		});
-
-		return deferred.promise();
-	}
-
-	function _showTiles(element) {
-		var array = [];
-		for (var i = 0; i < element.find(">div.tile").length; i++)
-			array.push(i);
-
-		array = _shuffle(array);
-		for (var i = 0; i < array.length; i++) {
-			var tile = element.find(">div.tile:eq(" + array[i] + ")").css("visibility", "hidden");
-			(function(innerTile) {
-				IssueTracker.Defer.execute(function() {
-					innerTile.css("visibility", "visible").hide().fadeIn(250);
-				}, 50 * (i + 1));
-			})(tile);
-		}
-	}
-
-	function _shuffle(array) {
-		for (var j, x, i = array.length; i; j = Math.floor(Math.random() * i), x = array[--i], array[i] = array[j], array[j] = x) ;
-		return array;
 	}
 
 })();
