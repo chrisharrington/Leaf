400bd244ba6d315aa58f9f6f042ee59aee2445dd
OpenShift guest <chrisharrington99@gmail.com>
2014-03-18 21:58:47 -0400

    Finished the create project stuff.

--- createProject.js
+++ createProject.js
@@ -6,21 +6,92 @@ var _all = require("node-promise").all;
 var Promise = require("node-promise").Promise;
 
 (function() {
-	// _openDatabaseConnection(_config.databaseConnectionString, function(connection) {
-	// 	_createProject(function(project) {
-	// 		_all(_createMilestones(project)).then(function() {
-	// 			connection.close();
-	// 			console.log("Connection to database closed.");	
-	// 		});
-	// 	});
-	// });
-	
-	
+	_openDatabaseConnection(_config.databaseConnectionString, function(connection) {
+		_createProject(function(project) {
+			_createUser(project, function(user) {
+				_all(_createMilestones(project), _createPriorities(project), _createStatuses(project), _createIssueTypes(project)).then(function() {
+					connection.close();
+					console.log("Connection to database closed.");	
+				});
+			});
+		});
+	});
 })();
 
-require("./data/models/milestone").find(function(error, milestones) {
-	console.log(milestones);
-});
+function _createUser(project, callback) {
+	_models.User.remove(function() {
+		new _models.User({ name: "Chris Harrington", emailAddress: "chrisharrington99@gmail.com", project: project._id }).save(function(error, user) {
+			if (error)
+				console.log("Could not create user: " + error);
+			else {
+				console.log("Created one user.");
+				callback(user);
+			}
+		})
+	});
+}
+
+function _createIssueTypes(project) {
+	var promise = new Promise();
+	_models.IssueType.remove(function() {
+		_all(
+			new _models.IssueType({ name: "Defect", project: project._id }).save(),
+			new _models.IssueType({ name: "Feature", project: project._id }).save(),
+			new _models.IssueType({ name: "Investigation", project: project._id }).save()
+		).then(function() {
+			console.log("Created three issue types.");
+			promise.resolve();
+		});
+	});
+	return promise;
+}
+
+function _createStatuses(project) {
+	var promise = new Promise();
+	_models.Status.remove(function() {
+		var pendingDevelopment, inDevelopment, pendingTesting, inTesting, failedTesting, complete;
+		_all(
+			new _models.Priority({ name: "Pending Development", order: 1, project: project._id }).save(function(e, m) { pendingDevelopment = m; }),
+			new _models.Priority({ name: "In Development", order: 2, project: project._id }).save(function(e, m) { inDevelopment = m; }),
+			new _models.Priority({ name: "Pending Testing", order: 3, project: project._id }).save(function(e, m) { pendingTesting = m; }),
+			new _models.Priority({ name: "In Testing", order: 4, project: project._id }).save(function(e, m) { inTesting = m; }),
+			new _models.Priority({ name: "Failed Testing", order: 5, project: project._id }).save(function(e, m) { failedTesting = m; }),
+			new _models.Priority({ name: "Complete", order: 6, project: project._id }).save(function(e, m) { complete = m; })
+		).then(function() {
+			console.log("Created six statuses.");
+			_models.Transition.remove(function() {
+				_all(
+					new _models.Transition({ name: "Start Development", from: pendingDevelopment._id, to: inDevelopment._id, project: project._id }),
+					new _models.Transition({ name: "Complete Development", from: inDevelopment._id, to: pendingTesting._id, project: project._id }),
+					new _models.Transition({ name: "Start Testing", from: pendingTesting._id, to: inTesting._id, project: project._id }),
+					new _models.Transition({ name: "Fail Testing", from: inTesting._id, to: failedTesting._id, project: project._id }),
+					new _models.Transition({ name: "Pass Testing", from: inTesting._id, to: complete, project: project._id }),
+					new _models.Transition({ name: "Restart Development", from: failedTesting._id, to: inDevelopment._id, project: project._id })
+				).then(function() {
+					console.log("Created six transitions.");
+					promise.resolve();
+				})
+			});
+		});
+	});
+	return promise;
+}
+
+function _createPriorities(project) {
+	var promise = new Promise();
+	_models.Priority.remove(function() {
+		_all(
+			new _models.Priority({ name: "Low", order: 1, project: project._id }).save(),
+			new _models.Priority({ name: "Medium", order: 2, project: project._id }).save(),
+			new _models.Priority({ name: "High", order: 3, project: project._id }).save(),
+			new _models.Priority({ name: "Critical", order: 4, project: project._id }).save()
+		).then(function() {
+			console.log("Created four priorities.");
+			promise.resolve();
+		});
+	});
+	return promise;
+}
 
 function _createMilestones(project) {
 	var promise = new Promise();
