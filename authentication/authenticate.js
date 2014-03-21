5c6d59b80cc51fd86b9f4b4ed7e34db5f11b9a2e
Chris Harrington <chrisharrington99@gmail.com>
2014-03-20 20:55:10 -0600

    Return a 401 when not authorized for issues list. Still need to add user to session somehow on sign in.

--- authentication/authenticate.js
+++ authentication/authenticate.js
@@ -1,5 +1,6 @@
 module.exports = function(request, response, next) {
-
-
-	next();
+	if (!request.cookies.staySignedIn || request.cookies.staySignedIn != "true")
+		response.send(401);
+	else
+		next();
 };
