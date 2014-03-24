a55ad4bc1f82a725678cb9531cbcb117612af5f3
Chris Harrington <chrisharrington99@gmail.com>
2014-03-23 22:21:06 -0600

    More javascript idempotent stuff.

--- authentication/authenticate.js
+++ authentication/authenticate.js
@@ -18,16 +18,6 @@ function _auth(request, response, next, updateSession) {
 		models.User.findOneAsync({ session: request.cookies.session }).then(function(user) {
 			if (user.expiration != null && user.expiration < Date.now())
 				response.send(401);
-			else if (updateSession) {
-				user.session = csprng(512, 36);
-				response.cookie("session", user.session, { expires: user.expiration });
-				return new Promise(function(resolve, reject) {
-					user.save(function(err) {
-						if (err) reject();
-						else resolve(user);
-					});
-				});
-			}
 		}).then(function(user) {
 			request.user = user;
 			next();
--- public/scripts/page.js
+++ public/scripts/page.js
@@ -1,3 +1,4 @@
+var IssueTracker = window.IssueTracker || {};
 
 IssueTracker.Page = function(params) {
 	this._validateParams(params);
