2824e5f927c494d9f0f8d6dd3940bb4b79c6a07f
Chris Harrington <chrisharrington99@gmail.com>
2014-03-25 10:53:30 -0600

    Added heroku setup stuff.

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 module.exports = function() {
+	if (!process.env.NODE_ENV)
+		process.env.NODE_ENV = "development";
 	return process.env.NODE_ENV == "production" ? _prod() : _dev();
-};
+}();
 
 function _dev() {
 	var settings = _defaults();
@@ -19,5 +21,5 @@ function _defaults() {
 		databaseConnectionString: "mongodb://IssueTrackerApp:C90BD87E-7267-4D55-B9A7-36B3581C3102@oceanic.mongohq.com:10038/issuetracker",
 		hashAlgorithm: "sha512",
 		dateFormat: "YYYY-MM-DD"
-	}
+	};
 }
