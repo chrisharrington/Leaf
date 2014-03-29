67d1f2f75a5ee0e41449e02dde67458ce1610c31
Chris Harrington <chrisharrington99@gmail.com>
2014-03-29 00:01:02 -0600

    Updated client-side notifications control to retrieve notifications on initialization from server, and to also pull down new notifications every minute.

--- public/scripts/notifications.js
+++ public/scripts/notifications.js
@@ -1,7 +1,27 @@
 (function(root) {
 
-    root.init = function() {
+	var NOTIFICATION_LOAD_INTERVAL = 60000;
 
+	root.loading = ko.observable(false);
+	root.notifications = ko.observableArray();
+
+    root.init = function() {
+		_loadNotifications();
+
+		setInterval(_loadNotifications, NOTIFICATION_LOAD_INTERVAL);
     };
+
+	function _loadNotifications() {
+		root.loading(true);
+		$.get(IssueTracker.virtualDirectory() + "notifications").done(function(notifications) {
+			root.notifications.removeAll();
+			for (var i = 0; i < notifications.length; i++)
+				root.notifications.push(IssueTracker.Utilities.createPropertyObservables(notifications[i]));
+		}).fail(function() {
+			IssueTracker.Feedback.error("An error has occurred while retrieving your notifications. Please contact technical support.");
+		}).always(function() {
+			root.loading(false);
+		});
+	}
 
 })(root("IssueTracker.Notifications"));
