3b3bdc5e2e88dead6c9fc3c81a7d76a022cb6a32
Chris Harrington <chrisharrington99@gmail.com>
2014-04-02 21:35:47 -0600

    Cleaned up the code for both bundler and scriptBundler.

--- bundling/bundler.js
+++ bundling/bundler.js
@@ -18,16 +18,29 @@ exports.renderCss = function() {
 
 exports.buildFileList = function(assets, files) {
 	return Promise.reduce(assets, function(list, asset) {
-		return fs.statAsync(asset).then(function(info) {
-			if (!info.isDirectory()) {
-				list.push(asset);
-				return list;
-			} else
-				return fs.readdirAsync(asset).then(function(newAssets) {
-					return exports.buildFileList(newAssets.map(function(curr) {
-						return asset + "/" + curr;
-					}), list);
-				});
-		});
+		return _stat(asset, list);
 	}, files || []);
 };
+
+function _stat(asset, list) {
+	return fs.statAsync(asset).then(function(info) {
+		return _handleAsset(info, list, asset);
+	});
+}
+
+function _handleAsset(info, list, asset) {
+	if (!info.isDirectory()) {
+		list.push(asset);
+		return list;
+	}
+
+	return _addFilesFromDirectory(asset, list);
+}
+
+function _addFilesFromDirectory(asset, list) {
+	return fs.readdirAsync(asset).then(function(newAssets) {
+		return exports.buildFileList(newAssets.map(function(curr) {
+			return asset + "/" + curr;
+		}), list);
+	});
+}
--- bundling/scriptBundler.js
+++ bundling/scriptBundler.js
@@ -5,28 +5,44 @@ var fs = Promise.promisifyAll(require("fs"));
 
 exports.render = function(directory, app) {
 	var files = [];
-	return fs.readdirAsync(directory).then(function(scripts) {
-		return bundler.buildFileList(scripts.map(function(curr) {
-			return "./public/scripts/" + curr;
-		}), files);
-	}).then(function () {
-		var promise = Promise.reduce(files.map(function (file) {
-			return app.get("env") == "production" ? fs.readFileAsync(file) : "<script type=\"text/javascript\" src=\"" + file.replace("./public", "") + "\"></script>\n";
-		}), function (result, rendered) {
-			return result + rendered;
-		}, "");
+	return _readDirectory(directory, files).then(function () {
+		var promise = _buildInitialPromise(files, app);
 		if (app.get("env") == "production")
-			promise = promise.then(function (concatenated) {
-				return minifier.compressAsync(concatenated, { type: "js" }).then(function (result) {
-					var script = result[0];
-					app.get("/script", function (request, response) {
-						response.writeHead(200, { "Content-Type": "text/javascript" });
-						response.write(script);
-						response.end();
-					});
-					return "<script type=\"text/javascript\" src=\"/script\"></script>";
-				});
-			});
+			promise = _handleProduction(promise, app);
 		return promise;
 	});
 };
+
+function _handleProduction(promise, app) {
+	return promise.then(function (concatenated) {
+		return minifier.compressAsync(concatenated, { type: "js" });
+	}).then(function (result) {
+		var script = result[0];
+		_setScriptRoute(app, script);
+		return "<script type=\"text/javascript\" src=\"/script\"></script>";
+	});
+}
+
+function _setScriptRoute(app, script) {
+	app.get("/script", function (request, response) {
+		response.writeHead(200, { "Content-Type": "text/javascript" });
+		response.write(script);
+		response.end();
+	});
+}
+
+function _buildInitialPromise(files, app) {
+	return Promise.reduce(files.map(function (file) {
+		return app.get("env") == "production" ? fs.readFileAsync(file) : "<script type=\"text/javascript\" src=\"" + file.replace("./public", "") + "\"></script>\n";
+	}), function (result, rendered) {
+		return result + rendered;
+	}, "");
+}
+
+function _readDirectory(directory, files) {
+	return fs.readdirAsync(directory).then(function(scripts) {
+		return bundler.buildFileList(scripts.map(function (curr) {
+			return "./public/scripts/" + curr;
+		}), files);
+	});
+}
