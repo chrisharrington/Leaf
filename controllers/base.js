38150d627cb0179b107e77be5ab1b00423007d06
Chris Harrington <chrisharrington99@gmail.com>
2014-04-03 20:23:21 -0600

    Refactored the base controller class.

--- controllers/base.js
+++ controllers/base.js
@@ -1,23 +1,24 @@
-var _mustache = require("mustache");
-var _fs = require("fs");
+var Promise = require("bluebird");
+var fs = Promise.promisifyAll(require("fs"));
+var mustache = require("mustache");
 
 var _rootContent;
 
 exports.inject = function(html) {
-    var root = _getRootContent(function(root) {
-        response.writeHead(200, { "Content-Type": "text/html" });
-        response.write(_mustache.render(root, { body: html }));
-        response.end();
-    });
+	_getRootContent().then(function(content) {
+		response.send(mustache.render(root, { body: html }));
+	}).catch(function(e) {
+		var message = "Error while rendering root content: " + e;
+		console.log(message);
+		response.send(message, 500);
+	});
 };
 
-function _getRootContent(callback) {
-    if (_rootContent)
-        callback(_rootContent);
-    else {
-        _fs.readFile("../view/root.html", function(err, content) {
-            _rootContent = content;
-            callback(_rootContent);
-        });
-    }
+function _getRootContent() {
+	if (_rootContent)
+		return Promise.resolve(_rootContent);
+
+	return fs.readFileAsync("../view/root.html").then(function(content) {
+		return _rootContent = content;
+	});
 }
