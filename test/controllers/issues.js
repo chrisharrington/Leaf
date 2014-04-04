da054c9b67d097705984101f04cc09b847e61c5b
Chris Harrington <chrisharrington99@gmail.com>
2014-04-04 11:11:29 -0600

    Added tests for the get /issues route.

--- test/controllers/issues.js
+++ test/controllers/issues.js
@@ -4,38 +4,72 @@ var fs = Promise.promisifyAll(require("fs"));
 
 var sut = require("../../controllers/issues");
 
-//describe("issues", function() {
-//	describe("get-issues", function() {
-//		it("should set get /issues route", function() {
-//			var app = { get: sinon.stub(), post: sinon.stub() };
-//			sut(app);
-//			assert(app.get.calledWith("/issues", sinon.match.func, sinon.match.func));
-//		});
-//
-//		it("should read issues.html file contents", function() {
-//			var content = "issues.html content";
-//			sinon.stub(fs, "readFileAsync").resolves(content);
-//
-//			_run({ verb: "get", route: "/issues" });
-//
-//			assert(fs.readFileAsync.calledWith("public/views/issues.html"));
-//		});
-//
-//		function _run(params) {
-//			var request = sinon.stub(), response = sinon.stub();
-//			var app = {
-//				get: function(route, b, c) {
-//					if (params.verb == "get" && route == params.route)
-//						if (c) c(request, response); else b(request, response);
-//				},
-//				post: function(route, b, c) {
-//					if (params.verb == "post" && route == params.route)
-//						if (c) c(request, response); else b(request, response);
-//				}
-//			};
-//
-//			sut(app);
-//			return { request: request, response: response };
-//		}
-//	});
-//});
+describe("issues", function() {
+	describe("get-issues", function() {
+		it("should set get /issues route", function() {
+			var app = { get: sinon.stub(), post: sinon.stub() };
+			sut(app);
+			assert(app.get.calledWith("/issues", sinon.match.func, sinon.match.func));
+		});
+
+		it("should read issues.html file contents", function() {
+			var content = "issues.html content";
+			sinon.stub(fs, "readFileAsync").resolves(content);
+
+			return _run({
+				verb: "get",
+				route: "/issues",
+				assert: function() { assert(fs.readFileAsync.calledWith("public/views/issues.html")); }
+			}).finally(function() {
+				fs.readFileAsync.restore();
+			});
+		});
+
+		it("should send contents of issues.html via response", function() {
+			var content = "issues.html content";
+			sinon.stub(fs, "readFileAsync").resolves(content);
+
+			return _run({
+				verb: "get",
+				route: "/issues",
+				assert: function(stubs) { assert(stubs.response.send.calledWith(content)); }
+			}).finally(function() {
+				fs.readFileAsync.restore();
+			});
+		});
+
+		it("should send 500 and error message on error", function() {
+			var error = "oh noes! an error!";
+			sinon.stub(fs, "readFileAsync").rejects(error);
+
+			return _run({
+				verb: "get",
+				route: "/issues",
+				assert: function(stubs) { assert(stubs.response.send.calledWith("Error while reading issues view: Error: " + error, 500)); }
+			}).finally(function() {
+				fs.readFileAsync.restore();
+			});
+		});
+
+		function _run(params) {
+			var func;
+			var request = sinon.stub(), response = { send: sinon.stub() };
+			var app = {
+				get: function(route, b, c) {
+					if (params.verb == "get" && route == params.route)
+						if (c) func = c; else func = b;
+				},
+				post: function(route, b, c) {
+					if (params.verb == "post" && route == params.route)
+						if (c) func = c; else func = b;
+				}
+			};
+
+			sut(app);
+			return func(request, response).then(function() {
+				if (params.assert)
+					params.assert({ request: request, response: response });
+			});
+		}
+	});
+});
