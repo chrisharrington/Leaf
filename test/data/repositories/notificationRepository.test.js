82582d4f7114c2d62e3acbc40e33aea62f6702ed
Chris Harrington <chrisharrington99@gmail.com>
2014-04-12 06:57:15 -0600

    100% coverage for notificationRepository.markAsRead.

--- test/data/repositories/notificationRepository.test.js
+++ test/data/repositories/notificationRepository.test.js
@@ -38,6 +38,46 @@ describe("notificationRepository", function() {
 
 		afterEach(function() {
 			sut.get.restore();
-		})
+		});
+	});
+
+	describe("markAsRead", function() {
+		var _stubs = {};
+
+		it("should retrieve notification using given notification id", function() {
+			var notificationId = "the notification id used to retrieve the notification details";
+			return _run({
+				notificationId: notificationId
+			}).then(function() {
+				assert(_stubs.details.calledWith(notificationId));
+			});
+		});
+
+		it("should set notification.isViewed to true", function() {
+			var notification = { saveAsync: sinon.stub().resolves() };
+			return _run({
+				notification: notification
+			}).then(function() {
+				assert(notification.isViewed);
+			});
+		});
+
+		it("should call notification.save", function() {
+			return _run().then(function() {
+				assert(_stubs.save.calledOnce);
+			});
+		});
+
+		afterEach(function() {
+			for (var name in _stubs)
+				if (_stubs[name].restore)
+				_stubs[name].restore();
+		});
+
+		function _run(params) {
+			params = params || {};
+			_stubs.details = sinon.stub(sut, "details").resolves(params.notification || { saveAsync: _stubs.save = sinon.stub().resolves() });
+			return sut.markAsRead(params.notificationId || "the notification id");
+		}
 	});
 });
