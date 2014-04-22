991860e176b8a9a21bd2efcc6e0258c35a815934
Chris Harrington <chrisharrington99@gmail.com>
2014-04-22 15:11:03 -0600

    Added tests for string.endsWith.

--- test/extensions/string.test.js
+++ test/extensions/string.test.js
@@ -23,8 +23,25 @@ describe("string extensions", function() {
 		});
 
 		it("should replace spaces with hyphens (-)", function() {
-			var blah = "1 2 a b A B".formatForUrl();
 			assert("1 2 a b A B".formatForUrl() == "1-2-a-b-a-b");
+		});
+	});
+
+	describe("endsWith", function() {
+		it("should return true for 'boogity'.endsWith('ity')", function() {
+			assert("boogity".endsWith("ity"));
+		});
+
+		it("should return false with case insensitive check", function() {
+			assert("boogity".endsWith("ITY") == false);
+		});
+
+		it("should return false with missing value", function() {
+			assert("boogity".endsWith() == false);
+		});
+
+		it("should return false if value length is greater than string length", function() {
+			assert("b".endsWith("ahhhh") == false);
 		});
 	});
 });
