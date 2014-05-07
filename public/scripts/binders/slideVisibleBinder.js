6f103787ec5b2e29047e83d16b58679f95e931e8
Chris Harrington <chrisharrington99@gmail.com>
2014-05-07 12:17:35 -0600

    slideViewBinder.js now uses jquery.transit for its animation.

--- public/scripts/binders/slideVisibleBinder.js
+++ public/scripts/binders/slideVisibleBinder.js
@@ -1,13 +1,6 @@
 ko.bindingHandlers.slideVisible = {
-	init: function(element, valueAccessor) {
-		// Initially set the element to be instantly visible/hidden depending on the value
-		var value = valueAccessor();
-		$(element).toggle(ko.unwrap(value)); // Use "unwrapObservable" so we can handle values that may or may not be observable
-	},
 	update: function(element, valueAccessor) {
-		// Whenever the value subsequently changes, slowly fade the element in or out
 		var value = valueAccessor();
-		var speed = 250;
-		ko.unwrap(value) ? $(element).slideDown(speed) : $(element).slideUp(speed);
+		$(element).transition({ height: ko.unwrap(value.bool) ? 40 : 0 }, value.speed || 250);
 	}
 };
