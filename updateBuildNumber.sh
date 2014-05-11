939dd81232d8353ebecf44d7e0161376fecb7a7c
Chris Harrington <chrisharrington99@gmail.com>
2014-05-10 21:36:29 -0600

    Attempt #6.

--- updateBuildNumber.sh
+++ updateBuildNumber.sh
@@ -1,5 +1,7 @@
+echo "Starting build number update."
 curl -n -X PATCH https://api.heroku.com/apps/leaf-issue-tracker/config-vars \
 -H "Accept: application/vnd.heroku+json; version=3" \
 -H "Authorization: $HEROKU_API_KEY_BASE64" \
 -H "Content-Type: application/json" \
 -d '{"BUILD_NUMBER": $TRAVIS_BUILD_NUMBER }'
+echo "Build number updated."
