802e94c5c4447439decdb81f7ce836b94c66d05e
Chris Harrington <chrisharrington99@gmail.com>
2014-05-28 16:00:29 -0600

    The user permissions dialog now shows correct user permissions.

--- public/scripts/views/users/permissions.js
+++ public/scripts/views/users/permissions.js
@@ -1,11 +1,19 @@
 (function(root) {
 
+	var _permissions;
+
 	root.loading = ko.observable(false);
 	root.name = ko.observable();
 
 	root.load = function(user) {
+		_loadPermissions(user);
+
 		root.name(user.name());
 		IssueTracker.Dialog.load("user-permissions", root);
+	};
+
+	root.isPermissionEnabled = function(permission) {
+		return _permissions[permission.id()];
 	};
 
 	root.save = function() {
@@ -15,5 +23,12 @@
 	root.cancel = function() {
 		IssueTracker.Dialog.hide();
 	};
+
+	function _loadPermissions(user) {
+		_permissions = {};
+		$.each(user.permissions(), function(i, permission) {
+			_permissions[permission.permissionId] = true;
+		});
+	}
 
 })(root("IssueTracker.Users.Permissions"));
