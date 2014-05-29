41820accdf48c9d211ba13c9ccd3b7508482751d
Chris Harrington <chrisharrington99@gmail.com>
2014-05-29 14:12:35 -0600

    Added tests for tpermissions controller.

---
+++ test/controllers/permissions.test.js
@@ -0,0 +1,77 @@
+require("../setup");
+var should = require("should"), assert = require("assert"), sinon = require("sinon"), Promise = require("bluebird"), extend = require('node.extend');
+var base = require("./base.test");
+var bundler = Promise.promisifyAll(require("../../bundling/bundler"));
+var assets = require("../../bundling/assets");
+var repositories = require("../../data/repositories");
+
+var sut = require("../../controllers/permissions");
+
+describe("permissions", function() {
+	describe("post /permissions", function () {
+		it("should set post /permissions route", function () {
+			var app = { get: sinon.stub(), post: sinon.stub() };
+			sut(app);
+			assert(app.post.calledWith("/permissions", sinon.match.func));
+		});
+
+		it("should remove user permissions for given user", function () {
+			var userId = "the user id";
+			return _run({
+				userId: userId,
+				assert: function(result) {
+					assert(result.stubs.remove.calledWith(userId));
+				}
+			});
+		});
+
+		it("should add permissions for given user", function () {
+			var userId = "the user id", permissions = ["the permission id"];
+			return _run({
+				userId: userId,
+				permissionIds: permissions,
+				assert: function(result) {
+					assert(result.stubs.add.calledWith(userId, permissions))
+				}
+			});
+		});
+
+		it("should send 200", function() {
+			return _run({
+				assert: function(result) {
+					assert(result.response.send.calledWith(200));
+				}
+			});
+		});
+
+		it("should send 500 on error", function() {
+			return _run({
+				remove: sinon.stub(repositories.UserPermission, "removeAllForUser").rejects(new Error("oh noes!")),
+				assert: function(result) {
+					assert(result.response.send.calledWith(sinon.match.any, 500));
+				}
+			});
+		});
+
+		function _run(params) {
+			params = params || {};
+			return base.testRoute({
+				sut: sut,
+				verb: "post",
+				route: "/permissions",
+				env: params.env,
+				stubs: {
+					remove: params.remove || sinon.stub(repositories.UserPermission, "removeAllForUser").resolves(),
+					add: sinon.stub(repositories.UserPermission, "addPermissionsForUser").resolves()
+				},
+				request: {
+					body: {
+						userId: params.userId,
+						permissionIds: params.permissionIds || []
+					}
+				},
+				assert: params.assert
+			});
+		}
+	});
+});
