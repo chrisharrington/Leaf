9fc64c0e2e7aca5b9a8cd5356ef47c6456711e72
Chris Harrington <chrisharrington99@gmail.com>
2014-06-06 09:18:39 -0600

    Attempting to get production mode working on AWS.

--- controllers/style.js
+++ controllers/style.js
@@ -9,10 +9,12 @@ module.exports = function(app) {
 		return bundler.concatenate(assets.styles()).then(function(concatenated) {
 			return less.renderAsync(concatenated);
 		}).then(function(css) {
-			return app.get("env") == "production" ? minifier.compressAsync(css, { type: "css" }) : [css];
+			//return app.get("env") == "production" ? minifier.compressAsync(css, { type: "css" }) : [css];
+			return [css];
 		}).then(function(results) {
 			response.header("Content-Type", "text/css");
-			response.header("Cache-Control", app.get("env") == "production" ? "public, max-age=2592000000" : "private, no-cache, max-age=0");
+//			response.header("Cache-Control", app.get("env") == "production" ? "public, max-age=2592000000" : "private, no-cache, max-age=0");
+			response.header("Cache-Control", "public, max-age=2592000000");
 			response.send(results[0], 200);
 		}).catch(function(e) {
 			response.send(e.stack.formatStack(), 500);
