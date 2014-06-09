842334336b5fdf1440ae8e649aaad2c39b4e36ea
Chris Harrington <chrisharrington99@gmail.com>
2014-06-09 16:16:36 -0600

    Removing more deployment files.

--- updateBuildNumber.sh
+++
@@ -1,5 +0,0 @@
-curl -n -X PATCH https://api.heroku.com/apps/leaf-issue-tracker/config-vars \
--H "Accept: application/vnd.heroku+json; version=3" \
--H "Authorization: $HEROKU_API_KEY_BASE64" \
--H "Content-Type: application/json" \
--d "{ \"BUILD_NUMBER\": $TRAVIS_BUILD_NUMBER }"
