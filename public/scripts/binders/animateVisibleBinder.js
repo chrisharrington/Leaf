4cec87b3249612cabbda6a43a838edffeddafc24
Chris Harrington <chrisharrington99@gmail.com>
2014-06-19 23:07:10 -0600

    Started hooking up detailed info dropdowns.

--- public/scripts/binders/animateVisibleBinder.js
+++ public/scripts/binders/animateVisibleBinder.js
@@ -3,22 +3,28 @@ ko.bindingHandlers.animateVisible = {
 		var value = valueAccessor();
 		switch (value.animation) {
 			case "slide":
-				$(element).height(ko.unwrap(value.bool) ? $(element).height() : 0);
+				$(element).height(ko.unwrap(value.bool) ? $(element).height() : 0).toggle(ko.unwrap(value.bool));
 				break;
 			case "fade":
-				$(element).css("opacity", ko.unwrap(value.bool) ? $(element).css("opacity") : 0);
+				$(element).css("opacity", ko.unwrap(value.bool) ? $(element).css("opacity") : 0).toggle(ko.unwrap(value.bool));
 				break;
 		}
 	},
 
 	update: function(element, valueAccessor) {
 		var value = valueAccessor();
+		var bool = ko.unwrap(value.bool);
 		switch (value.animation) {
 			case "slide":
-				$(element).transition({ height: ko.unwrap(value.bool) ? value.height || "auto" : "0px" }, value.speed || 250);
+				if (bool)
+					$(element).show();
+				$(element).transition({ height: ko.unwrap(value.bool) ? value.height || "auto" : "0px" }, value.speed || 250, function() {
+					if (!bool)
+						$(element).hide();
+				});
 				break;
 			case "fade":
-				$(element).transition({ opacity: ko.unwrap(value.bool) ? value.opacity || 100 : 0 }, value.speed || 250);
+				$(element).toggle(ko.unwrap(value.bool)).transition({ opacity: ko.unwrap(value.bool) ? value.opacity || 100 : 0 }, value.speed || 250);
 				break;
 		}
 	}
---
+++ public/scripts/views/issueDetails/detailedInfo.js
@@ -0,0 +1,5 @@
+(function(root) {
+
+	root.milestonesVisible = ko.observable(false);
+
+})(root("IssueTracker.IssueDetails.DetailedInfo"));
