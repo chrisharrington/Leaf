21e09917f1cdf9fe1213879f6a01ac6c3621475d
Chris Harrington <chrisharrington99@gmail.com>
2014-06-21 07:12:16 -0600

    Hooked up accordion-style detailed information panels.

--- public/scripts/views/issueDetails/detailedInfo.js
+++ public/scripts/views/issueDetails/detailedInfo.js
@@ -11,24 +11,60 @@
 		_toggle("milestones");
 	};
 
+	root.selectMilestone = function(milestone) {
+		IssueTracker.selectedIssue.milestoneId(milestone.id());
+		IssueTracker.selectedIssue.milestone(milestone.name());
+		root.toggleMilestones();
+	};
+
 	root.togglePriorities = function() {
 		_toggle("priorities");
+	};
+
+	root.selectPriority = function(priority) {
+		IssueTracker.selectedIssue.priorityId(priority.id());
+		IssueTracker.selectedIssue.priority(priority.name());
+		root.togglePriorities();
 	};
 
 	root.toggleStatuses = function() {
 		_toggle("statuses");
 	};
 
+	root.selectStatus = function(status) {
+		IssueTracker.selectedIssue.statusId(status.id());
+		IssueTracker.selectedIssue.status(status.name());
+		root.toggleStatuses();
+	};
+
 	root.toggleIssueTypes = function() {
 		_toggle("issueTypes");
+	};
+
+	root.selectIssueType = function(type) {
+		IssueTracker.selectedIssue.typeId(type.id());
+		IssueTracker.selectedIssue.type(type.name());
+		root.toggleIssueTypes();
 	};
 
 	root.toggleDevelopers = function() {
 		_toggle("developers");
 	};
 
+	root.selectDeveloper = function(developer) {
+		IssueTracker.selectedIssue.developerId(developer.id());
+		IssueTracker.selectedIssue.developer(developer.name());
+		root.toggleDevelopers();
+	};
+
 	root.toggleTesters = function() {
 		_toggle("testers");
+	};
+
+	root.selectTester = function(tester) {
+		IssueTracker.selectedIssue.testerId(tester.id());
+		IssueTracker.selectedIssue.tester(tester.name());
+		root.toggleTesters();
 	};
 
 	function _toggle(property) {
