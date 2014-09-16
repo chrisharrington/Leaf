7ab8bb618d1d8f51c5970d81679f24bbe88036c5
Chris Harrington <chrisharrington99@gmail.com>
2014-09-16 13:34:51 -0600

    Gave up on fancy button, as it was too fancy.

--- public/scripts/directives/pressable.js
+++ public/scripts/directives/pressable.js
@@ -10,7 +10,9 @@ IssueTracker.app.directive("pressable", function($timeout) {
 			$(element).on("click", function(e) {
 				var el = $(this), button = el.find("button");
 				el.addClass("active animate");
-				el.find(".flash").css({ left: e.offsetX - button.outerWidth()/2 });
+
+				var flash = el.find(".flash");
+				flash.css({ width: button.outerWidth() + "px", left: e.offsetX - button.outerWidth()/2 });
 				$timeout(function() {
 					el.removeClass("animate active");
 				}, IssueTracker.ANIMATION_SPEED*2);
