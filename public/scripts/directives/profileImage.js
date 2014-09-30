e3cde7279b71843c2ee63da940a5de745880ac4e
Chris Harrington <chrisharrington99@gmail.com>
2014-09-29 19:24:49 -0600

    Profile image directive now will only show the user's name as a tooltip when requested.

--- public/scripts/directives/profileImage.js
+++ public/scripts/directives/profileImage.js
@@ -6,7 +6,8 @@ IssueTracker.app.directive("profileImage", function($rootScope, md5) {
 			size: "@",
 			id: "="
 		},
-		link: function(scope) {
+		link: function(scope, element, attributes) {
+			scope.hasTooltip = attributes.showTooltip !== undefined;
 			var user = $rootScope.users.dict("id")[scope.id];
 			scope.location = "http://gravatar.com/avatar/" + md5.createHash(user.emailAddress) + "?s=" + (scope.size || 35);
 			scope.name = user.name;
