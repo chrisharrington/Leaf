4d920824b04de5432b1976afa9d94677caba5614
Chris Harrington <chrisharrington99@gmail.com>
2014-09-30 22:32:58 -0600

    Deleted users now roll in and out.

--- public/scripts/directives/profileImage.js
+++ public/scripts/directives/profileImage.js
@@ -7,8 +7,6 @@ IssueTracker.app.directive("profileImage", function($rootScope, md5) {
 			id: "="
 		},
 		link: function(scope, element, attributes) {
-			console.log(scope.id);
-
 			scope.hasTooltip = attributes.showTooltip !== undefined;
 			var user = $rootScope.users.dict("id")[scope.id];
 			scope.location = "http://gravatar.com/avatar/" + md5.createHash(user.emailAddress) + "?s=" + (scope.size || 35) + "&d=mm";
