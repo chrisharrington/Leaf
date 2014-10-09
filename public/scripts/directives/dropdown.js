b07c0d261dc0d3d6d85ebbbc315ab614c7c73b89
Chris Harrington <chrisharrington99@gmail.com>
2014-10-08 21:47:34 -0600

    More issue UI.

--- public/scripts/directives/dropdown.js
+++ public/scripts/directives/dropdown.js
@@ -1,22 +1,29 @@
-IssueTracker.app.directive("dropdown", function() {
+IssueTracker.app.directive("dropdown", function($rootScope) {
 	return {
 		restrict: "E",
 		templateUrl: "templates/dropdown.html",
 		scope: {
-			selected: "=",
-			items: "=",
+            items: "=",
 			placeholder: "@",
 			ngModel: "="
 		},
 		link: function(scope, element, attributes) {
+            if (attributes.selected !== undefined)
+                scope.selected = scope.$eval(attributes.selected);
 			scope.listVisible = false;
-			scope.selected = scope.selected || {};
 			scope.showDeleted = attributes.showDeleted !== undefined;
 			
 			scope.select = function(item) {
 				scope.listVisible = false;
 				scope.selected = item;
-			}
+			};
+            
+            $rootScope.$on("documentClicked", function(context, target) {
+                if (!$(target).hasClass("dropdown-label"))
+                    scope.$apply(function() {
+                        scope.listVisible = false;
+                    });
+            });
 		}
 	};
 });
--- public/scripts/services/controls/issue.js
+++ public/scripts/services/controls/issue.js
@@ -1,10 +1,17 @@
 IssueTracker.app.service("issue", function() {
 	this.visible = false;
 	this.loading = false;
+    
+    this.milestone = {};
 	
 	this.show = function() {
 		this.visible = true;
 	};
+    
+    this.load = function(issue) {
+        this.issue = issue;
+        this.show();
+    };
 	
 	this.ok = function() {
 		alert("ok");
